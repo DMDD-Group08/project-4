26c278ded6851ebcb9c7321bcbc3f51de03de78d
yashbehara <beharayashwanth@gmail.com>
2024-04-09 18:51:36 -0400

    Added new View & updated seller script

--- seller_script.sql
+++ seller_script.sql
@@ -1,8 +1,15 @@
 SET SERVEROUTPUT ON;
- 
+
 -- Seller adds product
 VARIABLE category_id_ VARCHAR2(10);
-exec BUSINESS_MANAGER.add_product(:category_id_, 'Scanner', 28.99, TO_DATE('21-02-23','DD-MM-YY'), NULL, 'Electronics', 'WHO');
+VARIABLE category_if_exists NUMBER;
+--add_product(category_id_, name, price, mfg_date, exp_date, category_name, seller_id)
+exec BUSINESS_MANAGER.add_product(:category_id_, :category_if_exists, 'Scanner2', 28.99, TO_DATE('21-02-23','DD-MM-YY'), NULL, 'Electronics', '1001');
  
 -- Seller updates the status of the refund
-exec BUSINESS_MANAGER.UPDATE_SELLER_REFUND(9005, 'Yes');
+VARIABLE return_id_if_exists NUMBER;
+VARIABLE seller_contact_if_exists NUMBER;
+VARIABLE seller_return_combination_if_exists NUMBER;
+--UPDATE_SELLER_REFUND(return_id_if_exists, seller_contact_if_exists, seller_return_combination_if_exists, return_id, accept_yes_no, seller_contact)
+exec BUSINESS_MANAGER.UPDATE_SELLER_REFUND(:return_id_if_exists, :seller_contact_if_exists, :seller_return_combination_if_exists, '9011', 'NO', 8008066453);
+
