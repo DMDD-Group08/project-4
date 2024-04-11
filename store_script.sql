c721da34f5e2ec2fff098215e4a19205751aa409
Dev Mithunisvar Premraj <145174051+devmith-7@users.noreply.github.com>
2024-04-11 13:19:41 -0400

    Update store_script.sql

--- store_script.sql
+++ store_script.sql
@@ -2,3 +2,5 @@ SET SERVEROUTPUT ON;
 
 -- Store updates their availability for the returns
 exec BUSINESS_MANAGER.UPDATE_STORE_AVAILABILITY('ST002', 0); 
+
+EXEC BUSINESS_MANAGER.Get_Feedback_For_Store(8007425877);
