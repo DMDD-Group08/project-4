ad14e40fb945f472be1aa29a175a54bece1243a7
Dev Mithunisvar Premraj <145174051+devmith-7@users.noreply.github.com>
2024-04-11 13:54:59 -0400

    Update customer_script.sql

--- customer_script.sql
+++ customer_script.sql
@@ -5,5 +5,17 @@ SET SERVEROUTPUT ON;
 VARIABLE Available_Qty NUMBER;
 exec BUSINESS_MANAGER.create_return(:Available_Qty, 'Damaged product', 1, '2001', '5508');
 
--- customer submits feedback to the store
-exec BUSINESS_MANAGER.submit_feedback('C003', 'ST003', 2.5, 'Bad staff');
+-- customer views returned products to the store to give feedback to the store
+
+exec BUSINESS_MANAGER.get_returned_products('alice@gmail.com');
+
+-- customer submits feedback
+BEGIN
+  BUSINESS_MANAGER.submit_feedback(
+    p_store_phone => 8005551234,
+    p_customer_email => 'bob@gmail.com',
+    p_customer_rating => 2,
+    p_review => 'The store was bad, staff not helpful'
+  );
+END;
+/
